ea0ef5bed6558e3b9b6a0091d0078bb49d55dc3b
Hannah Lin <ht.techx@gmail.com>
2023-11-16 20:40:11 -0500

    setting up ejs layout headers

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 
@@ -22,6 +23,7 @@ db.once("open", () => {
 // Setting up Express application and Views:
 const app = express();
 
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
